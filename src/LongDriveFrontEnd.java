cf96681ac6fd0cfb2435190810ac7251f0f29e72
ZDeutch <zander.deutch@menloschool.org>
2025-04-28 11:48:14 -0700

    Made screen full size

--- src/LongDriveFrontEnd.java
+++ src/LongDriveFrontEnd.java
@@ -2,21 +2,21 @@ import javax.swing.*;
 import java.awt.*;
 
 public class LongDriveFrontEnd extends JFrame {
-    public final static int WINDOW_WIDTH = 800;
-    public final static int WINDOW_HEIGHT = 800;
+    public final static int WINDOW_WIDTH = 1500;
+    public final static int WINDOW_HEIGHT = 900;
     public final static Color BACKGROUND = new Color(255, 255, 255);
-    private Ball ball;
-    private LongDrive game;
-    private Image firstBackground;
-    private Image secondBackgroound;
-    private Image backSwing;
-    private Image throughSwing;
+    private final Ball ball;
+    private final LongDrive game;
+    private final Image firstBackground;
+    private final Image secondBackground;
+    private final Image backSwing;
+    private final Image throughSwing;
 
-    public LongDriveFrontEnd(LongDrive game, Ball b) {
+    public LongDriveFrontEnd(LongDrive game, Ball ball) {
         this.game = game;
-        ball = b;
+        this.ball = ball;
         firstBackground = new ImageIcon("Resources/LongDriveBackGround.png").getImage();
-        secondBackgroound = new ImageIcon("Resources/ZoomedOutBackground.png").getImage();
+        secondBackground = new ImageIcon("Resources/ZoomedOutBackground.png").getImage();
         backSwing = new ImageIcon("Resources/BackSwing.png").getImage();
         throughSwing = new ImageIcon("Resources/ThroughSwing.png").getImage();
 
@@ -31,15 +31,15 @@ public class LongDriveFrontEnd extends JFrame {
         g.setColor(BACKGROUND);
         g.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
         g.drawImage(firstBackground, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, this);
-        g.drawImage(backSwing,75,WINDOW_HEIGHT - 275,WINDOW_WIDTH / 4, 200, this);
+        g.drawImage(backSwing,Ball.STARTING_X,WINDOW_HEIGHT - 275,WINDOW_WIDTH / 4, WINDOW_HEIGHT / 4, this);
         g.setColor(Color.BLACK);
         ball.draw(g);
     }
     public void gameScreen2(Graphics g) {
         g.setColor(BACKGROUND);
         g.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-        g.drawImage(secondBackgroound, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, this);
-        g.drawImage(throughSwing,75,WINDOW_HEIGHT - 275,100, 100, this);
+        g.drawImage(secondBackground, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, this);
+        g.drawImage(throughSwing,Ball.STARTING_X,WINDOW_HEIGHT - 150,WINDOW_WIDTH / 8, WINDOW_HEIGHT / 8, this);
         g.setColor(Color.BLACK);
         ball.drawSecondScreen(g);
     }
